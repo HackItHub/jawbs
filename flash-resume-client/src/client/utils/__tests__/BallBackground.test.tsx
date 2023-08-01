13ca71f57f06c9d14a2e8ce34cc7c9beab0b2139
Sharjeel Iqbal <SharjeelSIqbal@gmail.com>
2023-08-01 16:38:05 -0700

    removed eslint and added test for transparent container

--- flash-resume-client/src/client/utils/__tests__/BallBackground.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import BallBackground from "../BallBackground";
-
-it("renders ball background", () => {
-  const component = renderer.create(<BallBackground />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
