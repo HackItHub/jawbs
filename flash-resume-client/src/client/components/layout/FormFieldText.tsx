eedb5685cf7c685e91da942cda2de32073520a30
Sharjeel Iqbal <SharjeelSIqbal@gmail.com>
2023-08-01 17:46:28 -0700

    Added text fields layout container

--- flash-resume-client/src/client/components/layout/FormFieldText.tsx
+++ flash-resume-client/src/client/components/layout/FormFieldText.tsx
@@ -2,21 +2,40 @@ import React, { useState } from "react";
 
 interface Props {
   placeholder: string;
-  required: boolean | undefined;
+  required: boolean;
+  label: string;
+  onChange: (e: string) => void;
+  id: string;
 }
 
-const FormFieldText: React.FC<Props> = ({ placeholder, required }) => {
+const FormFieldText: React.FC<Props> = ({
+  placeholder,
+  required,
+  label,
+  onChange,
+  id,
+}) => {
   const [input, setInput] = useState("");
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    onChange(e.target.value);
+  };
   return (
-    <input
-      onChange={(e) => setInput(e.target.value)}
-      className='text-input'
-      type='text'
-      placeholder={placeholder}
-      aria-required={required}
-      required={required}
-      value={input}
-    />
+    <div className='form-field'>
+      <label htmlFor={id}>
+        ${label}
+        <input
+          id={id}
+          onChange={handleInput}
+          className='text-input'
+          type='text'
+          placeholder={placeholder}
+          aria-required={required}
+          required={required}
+          value={input}
+        />
+      </label>
+    </div>
   );
 };
 
