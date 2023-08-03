9427113d339f86732b782cf22ee73f6d98c05dae
Sharjeel Iqbal <SharjeelSIqbal@gmail.com>
2023-08-02 23:31:10 -0700

    Set width of transparent container

--- flash-resume-client/src/client/components/layout/TransparentContainer.tsx
+++ flash-resume-client/src/client/components/layout/TransparentContainer.tsx
@@ -4,7 +4,7 @@ const TransparentContainer: React.FC<React.PropsWithChildren> = ({
   children,
 }) => {
   return (
-    <div className='bg-opacity-50 bg-white drop-shadow-md'>
+    <div className='bg-opacity-50 bg-white drop-shadow-md w-[600px]'>
       <div className='px-8 py-10'>{children}</div>
     </div>
   );
