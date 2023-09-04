cc7bef259027d21e5a41f6bc7eb1444b6dceb182
Sharjeel Iqbal <SharjeelSIqbal@gmail.com>
2023-09-04 04:35:39 -0700

    removed input from drop down field

--- flash-resume-client/src/client/components/layout/DropDownMenu.tsx
+++ flash-resume-client/src/client/components/layout/DropDownMenu.tsx
@@ -43,7 +43,6 @@ const DropDownMenu: React.FC<ChildProps> = ({
       <div className='form-input-container items-start' role='presentation'>
         {listName}
       </div>
-      <input type='text' value={option} style={{ display: "none" }} />
       <div
         className='relative inline-block text-left mb-2 w-full'
         id={listName.toLowerCase()}
