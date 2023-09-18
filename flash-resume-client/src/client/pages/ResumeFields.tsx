24e14b0fb6d4123f09ad708c78f9f45330597511
Sharjeel Iqbal <SharjeelSIqbal@gmail.com>
2023-09-18 00:17:21 -0700

    Added animated forms

--- flash-resume-client/src/client/pages/ResumeFields.tsx
+++ flash-resume-client/src/client/pages/ResumeFields.tsx
@@ -1,19 +1,51 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import "../assets/styles/form.css";
-import { Education } from "../components/resume-fields";
+import {
+  Education,
+  Experience,
+  Person,
+  Address,
+} from "../components/resume-fields";
 
 const ResumeFields: React.FC = () => {
   const [formIndex, setFormIndex] = useState(0);
   const handleFormChange = () => {
-    setFormIndex(formIndex + 1);
+    setFormIndex((formIndex + 1) % 4);
   };
 
-  return (
-    <motion.div animate={{ translateX: "20px" }}>
+  const forms = [
+    <motion.div
+      animate={{ x: 0 }}
+      initial={{ x: "-100%" }}
+      exit={{ x: "100%" }}
+    >
+      <Person handleFormChange={handleFormChange} />
+    </motion.div>,
+    <motion.div
+      animate={{ x: 0 }}
+      initial={{ x: "-100%" }}
+      exit={{ x: "100%" }}
+    >
+      <Address handleFormChange={handleFormChange} />
+    </motion.div>,
+    <motion.div
+      animate={{ x: 0 }}
+      initial={{ x: "-100%" }}
+      exit={{ x: "100%" }}
+    >
       <Education handleFormChange={handleFormChange} />
-    </motion.div>
-  );
+    </motion.div>,
+    <motion.div
+      animate={{ x: 0 }}
+      initial={{ x: "-100%" }}
+      exit={{ x: "100%" }}
+    >
+      <Experience handleFormChange={handleFormChange} />
+    </motion.div>,
+  ];
+
+  return <AnimatePresence>{forms[formIndex]}</AnimatePresence>;
 };
 
 export default ResumeFields;
