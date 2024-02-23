0c19ae8e4c4945083c0b617128bc0b7c98172f0a
Sharjeel Iqbal <=>
2024-02-23 15:53:19 -0800

    Switched webpack to vite for easy usage and faster compiling times

--- jawbs-client/vite.config.ts
+++ jawbs-client/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslint from "vite-plugin-eslint";
 import dns from "dns";
 
 dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
   server: {
     port: 3000,
     hmr: {
