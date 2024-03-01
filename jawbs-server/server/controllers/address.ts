e7f403dd847017f52307d28a461675adbcba0672
Sharjeel Iqbal <=>
2024-03-01 11:52:58 -0800

    #17 updated address to fit token format

--- jawbs-server/server/controllers/address.ts
+++ jawbs-server/server/controllers/address.ts
@@ -1,9 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import prisma from "../libs/prisma.js";
+import ClientError from "../libs/client-error.js";
 
 const create = async (req: Request, res: Response, next: NextFunction) => {
+  const userId = req.user ? req.user.id : undefined;
+
+  if (!userId) {
+    throw new ClientError(400, "invalid credentials");
+  }
+
+  const address = { ...req.body, userId };
+
   try {
-    const newAddress = await prisma.address.create({ data: req.body });
+    const newAddress = await prisma.address.create({ data: address });
     res.status(201).json(newAddress);
   } catch (err) {
     next(err);
@@ -12,47 +21,62 @@ const create = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-const read = async (req: Request, res: Response) => {
+const read = async (req: Request, res: Response, next: NextFunction) => {
+  const userId = req.user ? req.user.id : undefined;
+
+  if (!userId) {
+    throw new ClientError(400, "invalid credentials");
+  }
+
   try {
-    const { id } = req.params;
     const address = await prisma.address.findUnique({
       where: {
-        id,
+        userId,
       },
     });
+    if (!address) {
+      throw new ClientError(404, "user not found");
+    }
+
     res.status(200).json(address);
   } catch (err) {
-    res.status(400).json({ message: err });
+    next(err);
   } finally {
     await prisma.$disconnect();
   }
 };
 
-const update = async (req: Request, res: Response) => {
+const update = async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.user ? req.user.id : undefined;
+
+  if (!id) {
+    throw new ClientError(400, "invalid credentials");
+  }
+
   try {
-    const { id } = req.params;
-    const updates = await req.body;
+    const updates = req.body;
     const updatedUser = await prisma.address.update({
       where: { id },
       data: { ...updates },
     });
     res.status(200).json(updatedUser);
   } catch (err) {
-    res.status(400).json({ message: err });
+    next(err);
   } finally {
     await prisma.$disconnect();
   }
 };
 
-const destroy = async (req: Request, res: Response) => {
+const destroy = async (req: Request, res: Response, next: NextFunction) => {
+  const id = req.user ? req.user.id : undefined;
   try {
-    const { id } = req.params;
     const deletedUser = await prisma.address.delete({
       where: { id },
     });
+
     res.status(200).json(deletedUser);
   } catch (err) {
-    res.status(400).json({ message: err });
+    next(err);
   } finally {
     await prisma.$disconnect();
   }
